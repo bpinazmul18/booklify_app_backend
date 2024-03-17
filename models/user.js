e3b3ec5af37cbb7c692074ff6e9ae4ff8996972a
Nazmul Haque <bpinazmul18@gmail.com>
2024-03-17 11:53:48 +0600

    fix: password length

--- models/user.js
+++ models/user.js
@@ -8,20 +8,20 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         minlength: 5,
-        maxlength: 50,
+        maxLength: 50,
     },
     email: {
         type: String,
         required: true,
         minlength: 5,
-        maxlength: 255,
+        maxLength: 255,
         unique: true,
     },
     password: {
         type: String,
         required: true,
-        minlength: 5,
-        maxlength: 1024
+        minlength: 4,
+        maxLength: 1024
     },
     isAdmin: Boolean,
 })
@@ -37,7 +37,7 @@ function validateUser(user) {
     const schema = Joi.object({
         name: Joi.string().min(5).max(50).required(),
         email: Joi.string().min(5).max(255).required().email(),
-        password: Joi.string().min(5).max(255).required()
+        password: Joi.string().min(4).max(255).required()
     })
 
     return schema.validate(user)
